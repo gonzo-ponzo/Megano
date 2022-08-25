1df0c2c05e6f54762cfded1131e6e9db9a8ee060
ya-andrey92 <ya.andrey92@gmail.com>
2022-08-25 13:38:59 +0300

    Add models in app shop
    change model Shop

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,22 @@
-from django.db import models
+from timestamps.models import models, Model
+from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
 
-class Shop(models.Model):
+class Shop(Model):
     """Магазин"""
     name = models.CharField(max_length=512, verbose_name=_("название"))
+    description = models.TextField(blank=True, verbose_name=_("описание"))
+    phone = models.CharField(max_length=20, verbose_name=_("телефон"))
+    email = models.CharField(max_length=100, verbose_name=_("email"))
+    address = models.CharField(max_length=512, verbose_name=_("адрес"))
+    image = models.ImageField(blank=True, upload_to="static/img/shop", verbose_name=_("фото"))
+    user = models.OneToOneField(User, blank=True, null=True, on_delete=models.PROTECT,
+                                verbose_name=_("пользователь"))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("магазин")
+        verbose_name_plural = _("магазины")
