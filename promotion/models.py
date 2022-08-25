ab7bc8ef3ab37aef8da91b3a8e05dc5369cd86b7
ya-andrey92 <ya.andrey92@gmail.com>
2022-08-25 13:43:02 +0300

    Add models in app promotion
    add model Banner

--- promotion/models.py
+++ promotion/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from timestamps.models import models, Model
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from product.models import Product
+
+
+class Banner(Model):
+    """Баннер"""
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, verbose_name=_("продукт"))
+    name = models.CharField(max_length=255, verbose_name=_("название"))
+    description = models.TextField(blank=True, verbose_name=_("описание"))
+    image = models.ImageField(upload_to="static/img/banner", verbose_name=_("баннер"))
+    is_active = models.BooleanField(default=False, verbose_name=_("активный"))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("баннер")
+        verbose_name_plural = _("баннеры")
