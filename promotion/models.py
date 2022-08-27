b8b627447cb10510b5f9245633fe9b2022bbe128
ya-andrey92 <ya.andrey92@gmail.com>
2022-08-27 16:01:14 +0300

    change in models soft delete

--- promotion/models.py
+++ promotion/models.py
@@ -1,10 +1,10 @@
-from timestamps.models import models, Model
+from timestamps.models import models, Timestampable
 from django.utils.translation import gettext_lazy as _
 
 from product.models import Product
 
 
-class Banner(Model):
+class Banner(Timestampable):
     """Баннер"""
     product = models.ForeignKey(Product, on_delete=models.PROTECT, verbose_name=_("продукт"))
     name = models.CharField(max_length=255, verbose_name=_("название"))
