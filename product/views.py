17bf2c4c61f35c805ed153c63b807b222dd94e07
ya-andrey92 <ya.andrey92@gmail.com>
2022-08-28 11:08:09 +0300

    add # noqa: F401

--- product/views.py
+++ product/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import CreateView
-from django.shortcuts import render
+from django.shortcuts import render  # noqa: F401
 from product.forms import ProductForm
 from product.models import Product
 from django.views.generic import TemplateView
