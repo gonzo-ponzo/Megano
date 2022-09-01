05bc0633da60773d303929c9aeb4066b73362f9c
ya-andrey92 <ya.andrey92@gmail.com>
2022-09-01 14:22:43 +0300

    app Shop - add admin and change model
    model Shop:
      - add admin
      - add in model validators email

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,7 @@
 from timestamps.models import models, Model
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import validate_email
 
 
 class Shop(Model):
@@ -9,7 +10,7 @@ class Shop(Model):
     name = models.CharField(max_length=512, verbose_name=_("название"))
     description = models.TextField(blank=True, verbose_name=_("описание"))
     phone = models.CharField(max_length=20, verbose_name=_("телефон"))
-    email = models.CharField(max_length=100, verbose_name=_("email"))
+    email = models.CharField(max_length=100, validators=[validate_email], verbose_name=_("email"))
     address = models.CharField(max_length=512, verbose_name=_("адрес"))
     image = models.ImageField(blank=True, upload_to="shop/%Y/%m/%d", verbose_name=_("фото"))
     user = models.OneToOneField(User, blank=True, null=True, on_delete=models.PROTECT, verbose_name=_("пользователь"))
