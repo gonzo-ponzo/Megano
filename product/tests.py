f5400a0afbfe742ad355110b099b58d0dffc82cc
ya-andrey92 <ya.andrey92@gmail.com>
2022-09-09 17:45:23 +0300

    develop model, admin and test ProductCategory
    models:
     - change on_deleted
     - add class TreeSoftDeleteManager
     - fixed method delete to remove active children
     - add classmethod for objects with count product
    
    admins:
     - change queryset
     - add column related_products_cumulative_count
    
    tests:
     - check delete parent with child

--- product/tests.py
+++ product/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from product.models import Product
+from product.models import Product, ProductCategory
 
 
 class ProductView(TestCase):
@@ -11,3 +11,20 @@ class ProductView(TestCase):
         response = self.client.post(path=path, data=data, content_type="application/json")
         self.assertEquals(response.status_code, 200)
         self.assertEquals(Product.objects.count(), 1)
+
+
+class ProductCategoryTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        parent_name = "parent"
+        parent = ProductCategory.objects.create(name=parent_name, slug=parent_name)
+        for i in range(3):
+            child_name = f"child{i}"
+            ProductCategory.objects.create(name=child_name, slug=child_name, parent=parent)
+
+    def test_check_delete_parent_with_child(self):
+        category = ProductCategory.objects
+        category.get(name="child1").delete()
+        self.assertEquals(category.count(), 3)
+        category.filter(name="parent").delete()
+        self.assertEquals(category.count(), 0)
