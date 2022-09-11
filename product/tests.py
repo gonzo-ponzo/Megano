9c8e215b6fe5cf56725f483a2a48a023cbff029d
Сергей Климов <s.klimov@bk.ru>
2022-09-11 01:58:53 +0000

    Update tests.py

--- product/tests.py
+++ product/tests.py
@@ -3,15 +3,6 @@ from django.urls import reverse
 from product.models import Product, ProductCategory
 
 
-class ProductView(TestCase):
-    def test_create_product(self):
-        path = reverse("product-create")
-        data = {"name": "Тестовый продукт"}
-        response = self.client.post(path=path, data=data, content_type="application/json")
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(Product.objects.count(), 1)
-
-
 class ProductCategoryTest(TestCase):
     @classmethod
     def setUpTestData(cls):
