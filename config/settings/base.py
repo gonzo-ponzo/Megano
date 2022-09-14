bd57266b1c43c5098070fa1c198c17c1a8082568
Константин Вечкаев <k.vechkaev@mail.ru>
2022-09-14 20:18:38 +0300

    merge, problem in base

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PROJ_DIR = Path(BASE_DIR).resolve().parent
 
 
 env = Env()
@@ -122,6 +123,7 @@ SESSION_COOKIE_AGE = 24 * 60 * 60
 WSGI_APPLICATION = "config.wsgi.application"
 
 
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -181,6 +183,7 @@ MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
