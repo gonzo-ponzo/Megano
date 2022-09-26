e723161b4f12dcb8763daefc43eb3750b8dc79c3
Константин Вечкаев <k.vechkaev@mail.ru>
2022-09-26 23:19:25 +0300

    add count products in compare

--- order/templatetags/cart.py
+++ order/templatetags/cart.py
@@ -1,5 +1,6 @@
 from django import template
 from order.cart import Cart
+from django.conf import settings
 
 register = template.Library()
 
@@ -12,3 +13,8 @@ def get_cart_len(request):
 @register.simple_tag()
 def get_cart_price(request):
     return Cart(request).get_total_price()
+
+
+@register.simple_tag()
+def get_compare_len(request):
+    return len(request.session.get(settings.CACHE_KEY_COMPARISON))
