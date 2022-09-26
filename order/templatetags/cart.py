ae277d2fb075bd16ce5ab585218460a581705a83
nikita_pruss <pruss.nikita@gmail.com>
2022-09-26 21:16:28 +0300

    cart reworked

--- order/templatetags/cart.py
+++ order/templatetags/cart.py
@@ -6,17 +6,11 @@ register = template.Library()
 
 @register.simple_tag()
 def get_cart_len(request):
-    if request.user.is_authenticated:
-        cart = Cart(request=request, user_cart=request.user.cart)
-    else:
-        cart = Cart(request)
+    cart = Cart(request)
     return len(cart)
 
 
 @register.simple_tag()
 def get_cart_price(request):
-    if request.user.is_authenticated:
-        cart = Cart(request=request, user_cart=request.user.cart)
-    else:
-        cart = Cart(request)
+    cart = Cart(request)
     return cart.get_total_price()
--- order/urls.py
+++ order/urls.py
@@ -4,7 +4,7 @@ from .views import cart_view, cart_add, cart_remove, cart_lower
 
 urlpatterns = [
     path("cart/", cart_view, name="cart-page"),
-    path("cart-add/<int:pk>/<int:shop_id>", cart_add, name="cart-add"),
-    path("cart-remove/<int:pk>/<int:shop_id>", cart_remove, name="cart-remove"),
-    path("cart-lower/<int:pk>/<int:shop_id>", cart_lower, name="cart-lower"),
+    path("cart-add/<str:product_id>/<str:shop_id>", cart_add, name="cart-add"),
+    path("cart-remove/<str:product_id>/<str:shop_id>", cart_remove, name="cart-remove"),
+    path("cart-lower/<str:product_id>/<str:shop_id>", cart_lower, name="cart-lower"),
 ]
