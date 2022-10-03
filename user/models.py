f4342c3029129bd4264438ee36a47dc32cb98f5b
gonzo <pruss.nikita@gmail.com>
2022-10-03 12:59:17 +0300

    fix warning test

--- user/models.py
+++ user/models.py
@@ -50,7 +50,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    cart = models.JSONField(default=dict())
+    cart = models.JSONField(default=dict)
 
     objects = CustomUserManager()
 
