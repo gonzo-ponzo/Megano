4c7d3d2eec02983462ed9fbfb541c40c8ce95ab2
Tany <arhiza@gmail.com>
2022-11-02 19:00:04 +0300

    logo for import

--- jobs/services.py
+++ jobs/services.py
@@ -1,6 +1,9 @@
+import os
 from pydantic import BaseModel, EmailStr, ValidationError, validator
 from phonenumber_field.phonenumber import PhoneNumber
 from decimal import Decimal
+from django.conf import settings
+from django.core.files import File
 
 from .models import Process
 from .tasks import shop_import
@@ -69,39 +72,58 @@ def one_shop_import(file_name):
     shop = Shop.objects.filter(email=email).first()
 
     fields = {"name", "description", "phone", "address"}
-    fields_requred = fields | {"user_mail"}
+    fields_requred = fields | {"user_mail", "logo_image"}
+    
+    logo_image = shop_data.logo_image
+    if logo_image:
+        logo_path = os.path.join(settings.IMPORT_INCOME, email, logo_image)
+    else:
+        logo_path = None
 
     user = None
     if shop_data.user_mail:
         user = get_user_model().objects.filter(email=shop_data.user_mail).first()
 
-    if shop:
+    if shop:  # редактирование информации о магазине
         for key in fields:
             if key in shop_data.__fields_set__:
                 setattr(shop, key, shop_data.__dict__.get(key))
         if user:
             shop.user = user
+        if logo_image and logo_image != os.path.basename(shop.image.name):
+            if os.path.isfile(logo_path):
+                shop.image = File(open(logo_path, 'rb'), name=logo_image)
+            else:
+                return False, f"Wrong logo path: {logo_path}"
         shop.save()
-    else:
+    else:  # добавление нового магазина
         need_fields = fields_requred - shop_data.__fields_set__
         if len(need_fields) > 0:
             return False, f"Required fields: {need_fields}"
+        # print(Shop.objects.filter(email=email).first())  # TODO совпадающий емейл может быть среди удаленных записей
+        if Shop.objects.filter(phone=shop_data.phone).first():
+            return False, f"Phone {shop_data.phone} is already used"
         if not user:
             return False, f"User with email {shop_data.user_mail} not found"
-        shop = Shop.objects.create(email=email, user=user, **{key: shop_data.__dict__.get(key) for key in fields})
-    error_list = []
-    for offer_data in data.offers:
-        product = Product.objects.filter(pk=offer_data.product_id).first()
-        if product:
-            offer = Offer.objects.filter(shop=shop, product=product).first()  # TODO update_or_create
-            if offer:
-                offer.price = offer_data.price
-                offer.amount = offer_data.amount
-                offer.save()
-            else:
-                Offer.objects.create(shop=shop, product=product, price=offer_data.price, amount=offer_data.amount)
+        if not os.path.isfile(logo_path):
+            return False, f"Wrong logo path: {logo_path}"
         else:
-            error_list.append(f"Product c offer_data.product_id={offer_data.product_id} not found")
+            image = File(open(logo_path, 'rb'), name=logo_image)
+        shop = Shop.objects.create(email=email, user=user, image=image, **{key: shop_data.__dict__.get(key) for key in fields})
+    error_list = []
+    if data.offers:
+        for offer_data in data.offers:
+            product = Product.objects.filter(pk=offer_data.product_id).first()
+            if product:
+                offer = Offer.objects.filter(shop=shop, product=product).first()  # TODO update_or_create
+                if offer:
+                    offer.price = offer_data.price
+                    offer.amount = offer_data.amount
+                    offer.save()
+                else:
+                    Offer.objects.create(shop=shop, product=product, price=offer_data.price, amount=offer_data.amount)
+            else:
+                error_list.append(f"Product c offer_data.product_id={offer_data.product_id} not found")
     return True, "TODO logo and shop-pictures"  # TODO
 
 
